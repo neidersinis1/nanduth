a34521305f3572dfbb326334dd8a75e22f3c7f94
Neider Sinisterra <neidersinis@hotmail.com>
2023-04-24 15:28:02 -0500

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
+// import { Toaster } from 'react-hot-toast';
+// import { SessionProvider } from 'next-auth/react';
+
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
+// import EditModal from '@/components/modals/EditModal';
+// import Modal from '@/components/Modal'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    {/* // <SessionProvider session={pageProps.session}> */}
+      {/* // <Toaster /> */}
+      // <RegisterModal />
+      // <LoginModal />
+      {/* // <EditModal /> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    {/* // </SessionProvider> */}
+    </>
+  )
 }
