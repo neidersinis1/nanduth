91409c559a8c345ea318ce713939fa9885f891a2
Neider Sinisterra <neidersinis@hotmail.com>
2023-07-11 12:31:03 -0500

    [TAILWIND] npm

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,13 +1,13 @@
-import Header from "@/components/Header";
-import SearchUsuario from "@/components/SearchUsuario"
+// import Header from "@/components/Header";
+// import SearchUsuario from "@/components/SearchUsuario"
 
-const Search = () => {
-  return ( 
-    <>
-      <Header label="Search" />
-      <SearchUsuario placeholder="Buscar Usuario" />
-    </>
-   );
-}
+// const Search = () => {
+//   return ( 
+//     <>
+//       <Header label="Search" />
+//       <SearchUsuario placeholder="Buscar Usuario" />
+//     </>
+//    );
+// }
  
-export default Search;
+// export default Search;
